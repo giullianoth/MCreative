c991c7437d3698a5a7fc7001e42ce6620a29c1af
Giulliano <llthguimaraes@gmail.com>
2023-05-19 10:07:41 -0300

    headline margin top on resize

--- assets/scripts/theme/headline-viewport.js
+++ assets/scripts/theme/headline-viewport.js
@@ -2,4 +2,5 @@ import { header, headline, setStyle } from "../variables.js";
 
 export default function HeadlineViewport() {
     setStyle(headline, "marginTop", `${header.offsetHeight}px`);
+    window.onresize = () => setStyle(headline, "marginTop", `${header.offsetHeight}px`);
 }
