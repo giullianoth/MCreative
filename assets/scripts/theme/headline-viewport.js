9ff2a5bec1be7c930cb4af6c1813c9c51e4d1dde
Giulliano <llthguimaraes@gmail.com>
2023-05-26 16:10:00 -0300

    html scroll padding top

--- assets/scripts/theme/headline-viewport.js
+++ assets/scripts/theme/headline-viewport.js
@@ -1,6 +1,11 @@
-import { header, headline, setStyle } from "../variables.js";
+import { getElement, header, headline, setStyle } from "../variables.js";
 
 export default function HeadlineViewport() {
     setStyle(headline, "marginTop", `${header.offsetHeight}px`);
-    window.onresize = () => setStyle(headline, "marginTop", `${header.offsetHeight}px`);
+    setStyle(getElement("html"), "scrollPaddingTop", `${header.offsetHeight}px`);
+
+    window.onresize = () => {
+        setStyle(headline, "marginTop", `${header.offsetHeight}px`);
+        setStyle(getElement("html"), "scrollPaddingTop", `${header.offsetHeight}px`);
+    }
 }
